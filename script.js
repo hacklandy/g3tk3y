c992e2f0713db75ef4d0fee8086d85b095b2df2b
rmt <92097362+hacklandy@users.noreply.github.com>
2024-08-30 06:57:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const key = urlParams.get('key');
-    const continueBtn = document.getElementById('continue-btn');
-    const keyContainer = document.getElementById('key-container');
-    const keySpan = document.getElementById('key');
-    const copyBtn = document.getElementById('copy-btn');
+const urlParams = new URLSearchParams(window.location.search);
+const key = urlParams.get('key');
+const continueBtn = document.getElementById('continue-btn');
+const keyContainer = document.getElementById('key-container');
+const keySpan = document.getElementById('key');
+const copyBtn = document.getElementById('copy-btn');
 
-    continueBtn.addEventListener('click', () => {
-        showFullscreenAd(); // Показываем полноэкранную рекламу
-    });
+continueBtn.addEventListener('click', () => {
+console.log('Кнопка продолжить нажата, пытаемся показать рекламу...');
+showFullscreenAd();
+});
 
-    copyBtn.addEventListener('click', () => {
-        navigator.clipboard.writeText(key).then(() => {
-            alert('The key copied!');
-        });
-    });
+copyBtn.addEventListener('click', () => {
+navigator.clipboard.writeText(key).then(() => {
+alert('The key copied!');
+});
+});
 
-    function showFullscreenAd() {
-        window.yaContextCb.push(() => {
-            Ya.Context.AdvManager.render({
-                blockId: "R-A-11786046-4",
-                type: "fullscreen",
-                platform: "touch",
-                onClose: () => {
-                    // После закрытия рекламы показываем ключ и кнопку "Скопировать"
-                    keyContainer.style.display = 'block';
-                    keySpan.textContent = key;
-                }
-            });
-        });
-    }
+function showFullscreenAd() {
+window.yaContextCb.push(() => {
+Ya.Context.AdvManager.render({
+blockId: "R-A-11786046-4",
+type: "fullscreen",
+platform: "touch",
+onRender: () => {
+console.log('Реклама отрендерилась');
+},
+onClose: () => {
+console.log('Реклама закрыта, показываем ключ и кнопку скопировать');
+keyContainer.style.display = 'block';
+keySpan.textContent = key;
+},
+onError: (error) => {
+console.error('Ошибка при показе рекламы:', error);
+}
+});
+});
+}
 });
