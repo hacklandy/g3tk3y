fd62fe5d981558b5844161d1e502def8b87927b3
rmt <92097362+hacklandy@users.noreply.github.com>
2024-08-30 06:50:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-const urlParams = new URLSearchParams(window.location.search);
-const key = urlParams.get('key');
-const continueBtn = document.getElementById('continue-btn');
-const keyContainer = document.getElementById('key-container');
-const keySpan = document.getElementById('key');
-const copyBtn = document.getElementById('copy-btn');
+    const urlParams = new URLSearchParams(window.location.search);
+    const key = urlParams.get('key');
+    const continueBtn = document.getElementById('continue-btn');
+    const keyContainer = document.getElementById('key-container');
+    const keySpan = document.getElementById('key');
+    const copyBtn = document.getElementById('copy-btn');
 
-continueBtn.addEventListener('click', () => {
-continueBtn.style.display = 'none';
-keyContainer.style.display = 'flex';
-keySpan.textContent = key;
-});
+    continueBtn.addEventListener('click', () => {
+        showFullscreenAd(); // Показываем полноэкранную рекламу
+    });
 
-copyBtn.addEventListener('click', () => {
-navigator.clipboard.writeText(key).then(() => {
-alert('The key copied!');
-});
-});
+    copyBtn.addEventListener('click', () => {
+        navigator.clipboard.writeText(key).then(() => {
+            alert('The key copied!');
+        });
+    });
+
+    function showFullscreenAd() {
+        window.yaContextCb.push(() => {
+            Ya.Context.AdvManager.render({
+                blockId: "R-A-11786046-4",
+                type: "fullscreen",
+                platform: "touch",
+                onClose: () => {
+                    // После закрытия рекламы показываем ключ и кнопку "Скопировать"
+                    keyContainer.style.display = 'block';
+                    keySpan.textContent = key;
+                }
+            });
+        });
+    }
 });
