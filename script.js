c1cfb128357673caa8668b64748dbf5e076284ad
rmt <92097362+hacklandy@users.noreply.github.com>
2024-08-30 05:39:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,37 +7,14 @@ const keySpan = document.getElementById('key');
 const copyBtn = document.getElementById('copy-btn');
 
 continueBtn.addEventListener('click', () => {
-showFullscreenAd().then(() => {
 continueBtn.style.display = 'none';
 keyContainer.style.display = 'block';
 keySpan.textContent = key;
 });
-});
 
 copyBtn.addEventListener('click', () => {
 navigator.clipboard.writeText(key).then(() => {
-alert('Ключ скопирован!');
+alert('The key copied!');
 });
 });
-
-function showFullscreenAd() {
-return new Promise((resolve) => {
-const adContainer = document.createElement('div');
-adContainer.id = 'fullscreen-ad-container';
-document.body.appendChild(adContainer);
-
-window.yaContextCb.push(() => {
-Ya.Context.AdvManager.render({
-"blockId": "R-A-11786046-7",
-"type": "fullscreen",
-"platform": "touch",
-"renderTo": "fullscreen-ad-container",
-"onClose": () => {
-document.body.removeChild(adContainer);
-resolve();
-}
-});
-});
-});
-}
 });
